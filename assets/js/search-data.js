02a104ac595620e8c3aa92f82489095acaa10e64
zherendong <dongzheren@gmail.com>
2026-01-14 22:47:47 +0000

    Deploying to gh-pages from @ zherendong/zherendong.github.io@568aa43361b2187f3f16398c073c8985e9a9363b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
